48976fe6be61cb35341af8cc2d027d0ebae9a33c
AnthemToTheNorth <ansovivan@mail.ru>
2023-11-17 19:24:40 +0700

    Task2

---
+++ src/com/homework/moduleTwo/task1/Main.java
@@ -0,0 +1,19 @@
+package com.homework.moduleTwo.task1;
+
+public class Main {
+
+    /*Проверка на четность*/
+
+    public static void main(String[] args) {
+
+        System.out.println(isParity(12));
+
+    }
+
+    public static boolean isParity(int a) {
+
+        return a % 2 == 0;
+
+    }
+
+}
