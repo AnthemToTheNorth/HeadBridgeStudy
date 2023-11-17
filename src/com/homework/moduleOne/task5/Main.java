b263de930c1af74988dc3ec1eeaf27ae438ce4c7
AnthemToTheNorth <ansovivan@mail.ru>
2023-11-17 20:45:22 +0700

    Task5

---
+++ src/com/homework/moduleOne/task5/Main.java
@@ -0,0 +1,25 @@
+package com.homework.moduleOne.task5;
+
+public class Main {
+
+    /*Найти факториал*/
+
+    public static void main(String[] args) {
+
+        System.out.println(findFactorial(5));
+
+    }
+
+    public static int findFactorial(int a){
+
+        int result = 1;
+
+        for (int i = 1; i <= a; i++) {
+            result = result * i;
+        }
+
+        return result;
+
+    }
+
+}
