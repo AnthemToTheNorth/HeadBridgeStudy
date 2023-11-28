d2cd843f99573203d241d892412f82152e01f7da
AnthemToTheNorth <ansovivan@mail.ru>
2023-11-28 22:08:37 +0700

    Five task

--- src/com/homework/secondModule/task5/Main.java
+++ src/com/homework/secondModule/task5/Main.java
@@ -1,4 +1,35 @@
 package com.homework.secondModule.task5;
 
+/*without returning an array*/
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5, 5, 5};
+        findDuplicates(arr);
+    }
+
+    public static void findDuplicates(int[] arr) {
+        int runner = arr[0];
+        boolean duplicateFound = false;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == runner) {
+                if (!duplicateFound) {
+                    System.out.print("Duplicates of " + runner + " found at ind: " + (i - 1));
+                    duplicateFound = true;
+                }
+                System.out.print(", " + i);
+            } else {
+                if (duplicateFound) {
+                    System.out.println();
+                    duplicateFound = false;
+                }
+                runner = arr[i];
+            }
+        }
+
+        if (duplicateFound) {
+            System.out.println();
+        }
+    }
 }
